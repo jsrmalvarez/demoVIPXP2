e356711a850de459878e495dc344847ad8d8d922
jsrmalvarez <jsrmalvarez@ninjatic.engineer>
2020-07-20 02:59:35 +0200

    Initial commit

---
+++ precache-manifest.0bb0f98b998c97b49c883c70e6bb19be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5de5fcd9084e0a0b6b89cc668290ae04",
+    "url": "/test-diagramas2/index.html"
+  },
+  {
+    "revision": "35e28d33e13eb48b3ef4",
+    "url": "/test-diagramas2/static/css/main.0c7f3690.chunk.css"
+  },
+  {
+    "revision": "46be7bd832a98758e84d",
+    "url": "/test-diagramas2/static/js/2.a5b1d2df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-diagramas2/static/js/2.a5b1d2df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35e28d33e13eb48b3ef4",
+    "url": "/test-diagramas2/static/js/main.39ce1b71.chunk.js"
+  },
+  {
+    "revision": "f2e8b33ea09338711a3a",
+    "url": "/test-diagramas2/static/js/runtime-main.9c02f2a4.js"
+  }
+]);
